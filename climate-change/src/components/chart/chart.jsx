12b5a60c47ba6e5f22ee0f12ee09211d06ef95b1
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-12-23 18:47:41 -0500

    changed chart data to be saved in state instead of a variable. Also added a button that does nothing so far

--- climate-change/src/components/chart/chart.jsx
+++ climate-change/src/components/chart/chart.jsx
@@ -3,47 +3,52 @@ import { Line } from 'react-chartjs-2';
 
 import './chart.css';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
 class climateChart extends React.Component {
     constructor(props) {
         super(props);
-        this.chartReference = React.createRef();
+        this.state = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                label: 'My First dataset',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40]
+                }
+            ]
+        };
     }
 
     componentDidMount() {
-        const { datasets } = this.refs.chart.chartInstance.data
-        console.log(datasets[0].data);
-      }
+        console.log(this.state.datasets[0].data);
+    }
     
     render() {
         return (
-            <Line ref="chart" data={data} />
+            <div>
+                <Line ref="chart" data={this.state} />
+                <button onClick={this.handleClick.bind(this)}>Update Data</button>
+            </div>
         )
+    }
+
+    handleClick() {
+        alert(1);
     }
 }
 
